734bfa4c81dd6e4d8b22a8e172ee456c4f23109c
rbeck4 <rbeck4@uw.edu>
2019-05-21 23:47:22 -0700

    Able to interlace additional pandas rows from additional files in specified csv list

--- dopedefects/data_build.py
+++ dopedefects/data_build.py
@@ -25,17 +25,44 @@ def init_pandas(data_dir, csvs):
             ignore_index=True)
     return data
 
+def clean_pandas(data):
+    """
+    """
+    entries = np.where(pandas.isnull(data['Bonds']) & pandas.isnull(\
+        data['Angles']) & pandas.isnull(data['Coulomb']))[0]
+    return data.drop(entries)
+
+def append_atomic_properties(data, file_in="Elemental_properties.csv"):
+    """
+    """
+    atomic_properties = pandas.read_csv(file_in)
+    for i in data.index.values:
+        atom = data.at[i, "M"]
+        try:
+            entry = atomic_properties.loc[(atomic_properties['Symbol'] == \
+                atom)].index.values[0]
+        except:
+            print("Atom '%s' not within the atomic_properties given, please \
+fill in missing values to %s, or correct input data." %(atom, file_in))
+            continue
+        for column in atomic_properties.columns.values:
+            data.at[i, column] = atomic_properties.at[entry, column]
+    return data
+
 def build_pandas(data_dir, csvs):
     """
     """
     data = init_pandas(data_dir, csvs)
     poscar_list = structure_extract.find_files(data_dir)
+    ##Predefine elements for pandas as are matricies instead of single
+    ##values.
     data["Bonds"] = None
     data["Bonds"] = data["Bonds"].astype(object)
     data["Angles"] = None
     data["Angles"] = data["Angles"].astype(object)
     data["Coulomb"] = None
     data["Coulomb"] = data["Coulomb"].astype(object)
+    ##
     for poscar in poscar_list:
         crystal = structure_extract.id_crystal(poscar)
         dopant = structure_extract.impurity_type(poscar)
@@ -43,7 +70,7 @@ def build_pandas(data_dir, csvs):
         if dopant == 'pure':
             dopant = site.replace("M_", "")
         try:
-            entry = data.loc[(data['CdX'] == crystal) & (data['M'] == dopant) & \
+            entry = data.loc[(data['CdX'] == crystal) & (data['M'] == dopant) &\
                 (data['Doping Site'] == site)].index[0]
         except:
             print("Cannot open dataframe entry for crystal %s, site %s, and \
@@ -60,6 +87,8 @@ for crystal %s, site %s and dopant %s.  Please check POSCAR"\
         data.at[entry, "Angles"] = angles
         data.at[entry, "Coulomb"] = structure_properties.coulomb(bonds, \
             structure_extract.geometry_defect(0, dopant, poscar))
+    data = clean_pandas(data)
+    data = append_atomic_properties(data)
     return data
 
 def save_pandas(data, file_name):
